d0830a5a112e853dc57f7ff5c60bbbca9cd3c848
Luizmrtz <luizgtvmartinez@gmail.com>
2025-11-15 20:02:37 -0300

    Terceira alteração teste

--- jogodaforca.c
+++ jogodaforca.c
@@ -44,11 +44,7 @@ int main(){
         return 0;
     }
 
-    printf("Helo World\n");
-    printf("Teste\n");
-    for(int i = 0; i < 2; i++){
-        printf("Teste aprovado!\n");
-    }
+
 
     return 0;
 }
